918406a8bb6ad202456376a3a3968cbfa942c74f
Abhishek Sharma <mex3yoursis@gmail.com>
2026-07-25 00:25:49 +0530

    Update weather.py

--- 13_weather_app/weather.py
+++ 13_weather_app/weather.py
@@ -1,1 +1,12 @@
+import requests
 
+class WeatherApp:
+    def __init__(self, city):
+        self.city = city
+
+    def show_weather(self):
+        # wttr.in se clean text fetch karke direct print
+        print(requests.get(f"https://wttr.in/{self.city}?format=%l:+%t,+%C").text)
+
+# Object creation and method call in one line
+WeatherApp(input("City: ")).show_weather()
