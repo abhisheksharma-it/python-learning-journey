ead997052f87093ffa19f6ee6686810d8da590d1
Abhishek Sharma <mex3yoursis@gmail.com>
2026-07-24 23:17:45 +0530

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-name = "Abhishek Sharma"
-age = 31
-price = 99.99
-
-
-print("my name is :" , name)
-print("my age is :", age)
-print(f"My name is {name}")
-print(f"My name is {name} and I am {age * 1} years old.")
